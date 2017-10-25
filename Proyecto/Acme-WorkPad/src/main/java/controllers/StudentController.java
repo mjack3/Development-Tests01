6d3ff094cb7707651b1384fee17cc89d9cb53648
mardancab <mardancab@01fd033c-ca7f-4c98-b007-f4806be563d6>
2017-10-25 13:35:11 +0000

    cambiado el pattern por como dice el documento de requisitos

--- Proyecto/Acme-WorkPad/src/main/java/controllers/StudentController.java
+++ Proyecto/Acme-WorkPad/src/main/java/controllers/StudentController.java
@@ -1,3 +1,4 @@
+
 package controllers;
 
 import java.util.regex.Matcher;
@@ -21,8 +22,9 @@ public class StudentController {
 
 	@Autowired
 	private StudentService	studentService;
-	
-	private Student toSave;
+
+	private Student			toSave;
+
 
 	public StudentController() {
 		super();
@@ -32,119 +34,108 @@ public class StudentController {
 	public ModelAndView create() {
 		ModelAndView result;
 
-		result = createNewModelAndView(studentService.create(), null);
+		result = this.createNewModelAndView(this.studentService.create(), null);
 
 		return result;
 	}
-	
+
 	@RequestMapping(value = "/edit", method = RequestMethod.GET)
 	public ModelAndView edit() {
 		ModelAndView result;
 
-		result = createEditModelAndView(studentService.checkPrincipal(), null);
+		result = this.createEditModelAndView(this.studentService.checkPrincipal(), null);
 
 		return result;
 	}
 
-	@RequestMapping(value="/save", method=RequestMethod.POST, params = "save")
-	public ModelAndView saveCreate(@Valid Student student, BindingResult binding) {
+	@RequestMapping(value = "/save", method = RequestMethod.POST, params = "save")
+	public ModelAndView saveCreate(@Valid final Student student, final BindingResult binding) {
 		ModelAndView result;
-		if (binding.hasErrors()) {
-			result = createNewModelAndView(student, null);
-		} else {
+		if (binding.hasErrors())
+			result = this.createNewModelAndView(student, null);
+		else
 			try {
-				Pattern pattern = Pattern.compile("^\\+([0-9][0-9]{0,2}) (\\([0-9][0-9][0-9]{0,3}\\)) ([a-zA-Z0-9 -]{4,})$");
-			    Matcher matcher = pattern.matcher(student.getPhone());
-				if(!matcher.matches()) {
+				final Pattern pattern = Pattern.compile("^([+])([0-9]{2})([ ])([0-9]{3})?([ ])?([0-9]{4,})$");
+				final Matcher matcher = pattern.matcher(student.getPhone());
+				if (!matcher.matches()) {
 					result = new ModelAndView("student/confirm");
-					toSave = student;
-				}else {
-					studentService.save(student);
+					this.toSave = student;
+				} else {
+					this.studentService.save(student);
 					result = new ModelAndView("redirect:/welcome/index.do");
-				}			
-			} catch (Throwable th) {
-				th.printStackTrace();
-				result = createNewModelAndView(student, "student.commit.error");
-		}
-		
-	}
-	return result;
-}
-	
-	@RequestMapping(value="/saveEdit", method=RequestMethod.POST, params = "save")
-	public ModelAndView saveEdit(@Valid Student student, BindingResult binding) {
-		ModelAndView result;
-		if (binding.hasErrors()) {
-			result = createEditModelAndView(student, null);
-		} else {
-			try {
-				Pattern pattern = Pattern.compile("^\\+([0-9][0-9]{0,2}) (\\([0-9][0-9][0-9]{0,3}\\)) ([a-zA-Z0-9 -]{4,})$");
-			    Matcher matcher = pattern.matcher(student.getPhone());
-				if(!matcher.matches()) {
-					result = new ModelAndView("student/confirm");
-					toSave = student;
-				}else {
-				studentService.update(student);
-				result = new ModelAndView("redirect:/welcome/index.do");
 				}
-			} catch (Throwable th) {
+			} catch (final Throwable th) {
 				th.printStackTrace();
-				result = createEditModelAndView(student, "student.commit.error");
-		}
-		
+				result = this.createNewModelAndView(student, "student.commit.error");
+			}
+		return result;
 	}
-	return result;
-}
-	
-	@RequestMapping(value="/saveConfirm", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/saveEdit", method = RequestMethod.POST, params = "save")
+	public ModelAndView saveEdit(@Valid final Student student, final BindingResult binding) {
+		ModelAndView result;
+		if (binding.hasErrors())
+			result = this.createEditModelAndView(student, null);
+		else
+			try {
+				final Pattern pattern = Pattern.compile("^([+])([0-9]{2})([ ])([0-9]{3})?([ ])?([0-9]{4,})$");
+				final Matcher matcher = pattern.matcher(student.getPhone());
+				if (!matcher.matches()) {
+					result = new ModelAndView("student/confirm");
+					this.toSave = student;
+				} else {
+					this.studentService.update(student);
+					result = new ModelAndView("redirect:/welcome/index.do");
+				}
+			} catch (final Throwable th) {
+				th.printStackTrace();
+				result = this.createEditModelAndView(student, "student.commit.error");
+			}
+		return result;
+	}
+
+	@RequestMapping(value = "/saveConfirm", method = RequestMethod.POST)
 	public ModelAndView saveConfirm() {
 		ModelAndView result;
-			try {
-				studentService.save(toSave);
-				result = new ModelAndView("redirect:/welcome/index.do");
-			} catch (Throwable th) {
-				th.printStackTrace();
-				result = createNewModelAndView(toSave, "student.commit.error");
+		try {
+			this.studentService.save(this.toSave);
+			result = new ModelAndView("redirect:/welcome/index.do");
+		} catch (final Throwable th) {
+			th.printStackTrace();
+			result = this.createNewModelAndView(this.toSave, "student.commit.error");
 		}
-		
-	return result;
-}
-	
-	@RequestMapping(value="/saveConfirmEdit", method=RequestMethod.POST)
+
+		return result;
+	}
+
+	@RequestMapping(value = "/saveConfirmEdit", method = RequestMethod.POST)
 	public ModelAndView saveConfirmEdit() {
 		ModelAndView result;
-			try {
-				studentService.update(toSave);
-				result = new ModelAndView("redirect:/welcome/index.do");
-			} catch (Throwable th) {
-				th.printStackTrace();
-				result = createNewModelAndView(toSave, "student.commit.error");
+		try {
+			this.studentService.update(this.toSave);
+			result = new ModelAndView("redirect:/welcome/index.do");
+		} catch (final Throwable th) {
+			th.printStackTrace();
+			result = this.createNewModelAndView(this.toSave, "student.commit.error");
 		}
-		
-	return result;
-}
 
-protected ModelAndView createNewModelAndView(Student student, String message) {
-	ModelAndView result;
-	result = new ModelAndView("student/create");
-	result.addObject("student", student);
-	result.addObject("message", message);
-	return result;
-}
+		return result;
+	}
 
-protected ModelAndView createEditModelAndView(Student student, String message) {
-	ModelAndView result;
-	result = new ModelAndView("student/edit");
-	result.addObject("student", student);
-	result.addObject("message", message);
-	return result;
-}
+	protected ModelAndView createNewModelAndView(final Student student, final String message) {
+		ModelAndView result;
+		result = new ModelAndView("student/create");
+		result.addObject("student", student);
+		result.addObject("message", message);
+		return result;
+	}
 
-
-
-
-
-
-
+	protected ModelAndView createEditModelAndView(final Student student, final String message) {
+		ModelAndView result;
+		result = new ModelAndView("student/edit");
+		result.addObject("student", student);
+		result.addObject("message", message);
+		return result;
+	}
 
 }
